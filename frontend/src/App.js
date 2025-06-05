869d5e5305ac26080248359f3fd91fb13ae68340
Raju Yarragoti <rajuyarragoti9@gmail.com>
2025-06-05 21:34:43 +0530

    Fix: Re-added frontend folder properly to main repo

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        🚗 Community Ride Sharing App
+      </Typography>
+
+    </Container>
+  );
+}
+
+export default App;
